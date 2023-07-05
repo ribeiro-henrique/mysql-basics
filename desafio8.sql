e2e66f022d6016441ae0ed3ec925de473c3cefb7
ribeiro-henrique <hesr.ribeiro@gmail.com>
2023-07-05 16:17:40 -0300

    req 8

--- desafio8.sql
+++ desafio8.sql
@@ -1,2 +1,2 @@
 SELECT CONCAT(first_name, '', last_name) as full_name,
-CONCAT(city, "-", state_province, ',', address) as location FROM employees;
+CONCAT(city, "-", state_province, ', ', address) as location FROM employees;
